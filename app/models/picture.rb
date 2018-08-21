c2437c5b7c1daf7cde357df60bd0da890ce08998
shizuka takada <takada.a4t@gmail.com>
2018-08-21 13:26:03 +0000

    画像バリデーションを追加

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,7 @@
 class Picture < ApplicationRecord
     mount_uploader :image, ImageUploader
     
+    validates :image, presence: true
     validates :content, length: { maximum: 140 }
     
     belongs_to :user
