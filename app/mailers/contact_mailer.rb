8bce39b84c8aa0277b9042233498939f2dce2616
shizuka takada <takada.a4t@gmail.com>
2018-08-22 13:08:06 +0000

    全角スペースを修正

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
-　def contact_mail(contact)
-    @contact = contact
-    mail to: 'kanri@gmail.com', subject: "お問い合わせ受付メール"
-　end
+    def contact_mail(contact)
+        @contact = contact
+        mail to: 'kanri@gmail.com', subject: "お問い合わせ受付メール"
+    end
 end
